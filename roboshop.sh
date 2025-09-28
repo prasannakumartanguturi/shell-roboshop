a967e69859bfd14825f61c14f2889fea38c0f8ec
prasanna <prasannakumardso@gmail.com>
2025-09-28 17:59:08 +0530

    commiting

--- roboshop.sh
+++ roboshop.sh
@@ -10,12 +10,11 @@ do
     instanceid=$( aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
 
     if [ $instance != "frontend" ]; then 
-        IP=$(aws ec2 describe-instances --instance-ids $instanceid --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
+        IP=$(aws ec2 describe-instances --instance-ids $instanceid --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
 
     else 
         IP=$(aws ec2 describe-instances --instance-ids $instanceid --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
 
     fi
-
+    echo $instance: $IP 
 done
-echo $instance: $IP 
