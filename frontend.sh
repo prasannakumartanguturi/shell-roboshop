83c5b6be0955728a358b5e59c25c6d0facf2475b
prasanna <prasannakumardso@gmail.com>
2025-09-28 16:32:27 +0530

    commiting

--- frontend.sh
+++ frontend.sh
@@ -2,3 +2,7 @@ echo "this is from shell-roboshop repo"
 
 instanceid=$( aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-0c0e9eb837218898b --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=teastmachine}]' --query 'Instances[0].InstanceId' --output text)
 echo "$instanceid"
+
+publicip=$(aws ec2 describe-instances --instance-ids $instanceid --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
+
+echo "publicip is: $publicip"
