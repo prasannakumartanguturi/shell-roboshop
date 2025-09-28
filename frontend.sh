7581c0e7752a7ec015de210a827653d122b81c40
prasanna <prasannakumardso@gmail.com>
2025-09-28 16:29:22 +0530

    commiting

--- frontend.sh
+++ frontend.sh
@@ -1,3 +1,4 @@
 echo "this is from shell-roboshop repo"
 
 instanceid=$( aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-0c0e9eb837218898b --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=teastmachine}]' --query 'Instances[0].InstanceId' --output text)
+echo "$instanceid"
