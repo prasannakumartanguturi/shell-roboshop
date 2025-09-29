a1f8b0ca67fabbd77547b0d9d3ee0223f4010491
prasanna <prasannakumardso@gmail.com>
2025-09-29 16:29:31 +0530

    commiting

--- frontend.sh
+++ frontend.sh
@@ -10,7 +10,7 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
-LOGS_FOLDER="/var/log/mongodb"
+LOGS_FOLDER="/var/log/nginx"
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-script/16-logs.log
 SCRIPT_DIR=$PWD
