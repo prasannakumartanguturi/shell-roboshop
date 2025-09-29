c8d0006c1d192b3c91ef203772af680af7fbb867
prasanna <prasannakumardso@gmail.com>
2025-09-29 15:27:30 +0530

    commiting

--- catalogue.sh
+++ catalogue.sh
@@ -74,7 +74,7 @@ cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongo.repo
 
 dnf install mongodb-mongosh -y
 # check inn google , shell script to check db,s are loaded or not.
-INDEX=$(mongosh mongodb.daws86s.fun --quiet --eval "db.getMongo().getDBNames().indexOf('catalogue')")
+INDEX=$(mongosh mongodb.prasannadso.fun --quiet --eval "db.getMongo().getDBNames().indexOf('catalogue')")
 if [ $INDEX -le 0 ]; then
     mongosh --host $MONGODB_HOST </app/db/master-data.js &>>$LOG_FILE
 else
