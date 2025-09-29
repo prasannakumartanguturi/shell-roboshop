4d3e4f6abb980a672ece1a966e7d9a1fcb5da467
prasanna <prasannakumardso@gmail.com>
2025-09-29 16:03:32 +0530

    commiting

--- frontend.sh
+++ frontend.sh
@@ -15,7 +15,7 @@ SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-script/16-logs.log
 SCRIPT_DIR=$PWD
 
-sudo mkdir -p $LOGS_FOLDER
+mkdir -p $LOGS_FOLDER
 echo "Script started executed at: $(date)" 
 
 if [ $USERID -ne 0 ]; then
