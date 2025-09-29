c61ee0c63ee023c7177c8c1467e1761cd027a5d5
prasanna <prasannakumardso@gmail.com>
2025-09-29 14:59:06 +0530

    commiting

--- catalogue.sh
+++ catalogue.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
+set -euo pipefail
+
+trap 'echo "There is an error in $LINENO, Command is: $BASH_COMMAND"' ERR
+
+
 USERID=$(id -u)
 R="\e[31m"
 G="\e[32m"
@@ -9,8 +15,10 @@ LOGS_FOLDER="/var/log/catalogue"
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-script/16-logs.log
 SCRIPT_DIR=$PWD
+MONGODB_HOST=mongodb.prasannadso.fun
 
-mkdir -p $LOGS_FOLDER
+
+mkdir -p $LOGS_FOLDER /app
 echo "Script started executed at: $(date)" | tee -a $LOG_FILE
 
 if [ $USERID -ne 0 ]; then
@@ -26,3 +34,50 @@ VALIDATE(){ # functions receive inputs through args just like shell script args
         echo -e " $2 ... $G SUCCESS $N" | tee -a $LOG_FILE
     fi
 }
+echo "disabling default version of nodejs and installling nodejs 20"
+
+dnf module disable nodejs -y
+dnf module enable nodejs:20 -y
+dnf install nodejs -y
+VALIDATE $? "installing nodejs"
+
+id roboshop
+if [ $? != 0 ]; then 
+    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop
+else 
+    echo "user already exists"
+fi
+
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip 
+VALIDATE $? "downloading catalogue code zip file from s3"
+
+cd /app 
+VALIDATE $? " changing to app directory"
+
+unzip /tmp/catalogue.zip
+
+cd /app 
+npm install 
+
+cp $PWD/catalogue.service /etc/systemd/system/catalogue.service
+
+systemctl daemon-reload
+systemctl enable catalogue 
+systemctl start catalogue
+
+echo -e "Catalogue application setup ... $G SUCCESS $N"
+
+
+cp $PWD/mongo.repo /etc/yum.repos.d/mongo.repo
+
+dnf install mongodb-mongosh -y
+# check inn google , shell script to check db,s are loaded or not.
+INDEX=$(mongosh mongodb.daws86s.fun --quiet --eval "db.getMongo().getDBNames().indexOf('catalogue')")
+if [ $INDEX -le 0 ]; then
+    mongosh --host $MONGODB_HOST </app/db/master-data.js &>>$LOG_FILE
+else
+    echo -e "Catalogue products already loaded ... $Y SKIPPING $N"
+fi
+
+systemctl restart catalogue
+echo -e "Loading products and restarting catalogue ... $G SUCCESS $N"
